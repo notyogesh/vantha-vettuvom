2f0c778ec14118813dc9754bdcc7f1e61fe6562a
Yogesh <yogeshreo@gmail.com>
2026-01-27 23:23:52 +0530

    dfdg

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://vanthavettuvom.com",
-  output: "server",
+  output: "static",
   adapter: vercel({
     edgeMiddleware: false,
   }),
